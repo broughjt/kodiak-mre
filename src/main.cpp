5c36d852368f05c56cc5c05c590e6cc8105caeff
Jackson Brough <jacksontbrough@gmail.com>
2026-06-22 17:17:50 -0600

    The Wilkinson polynomial example OOMs on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
+#include <Adapters/PRECiSA.hpp>
 #include <kodiak.hpp>
 
-#include <iostream>
+using namespace kodiak;
+using namespace kodiak::Adapters::PRECiSA;
 
 int main() {
     kodiak::Kodiak::init();
-    std::cout << "Hello from Kodiak!\n";
+
+    kodiak::MinMaxSystem system;
+
+    int d = 20;
+    const nat x_index = system.var("x", Interval(1, 20));
+    Real x = var(x_index, "x");
+    Real zero = val(Interval(0.0));
+    Real one = val(Interval(1.0));
+    Real p = x - one;
+    Real e = aebounddp_sub(x, zero, one, zero);
+
+    for (int i = 2; i <= d; ++i) {
+        Real pi = x - val(Interval(double(i)));
+        Real ei = aebounddp_sub(x, zero, val(Interval(double(i))), zero);
+        e = aebounddp_mul(p, e, pi, ei);
+        p = p * pi;
+    }
+
+    system.max(e);
+
     return 0;
 }
